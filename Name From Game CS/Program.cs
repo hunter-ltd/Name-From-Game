3a0981f3732a538a09b2cde20c48d10dc2723fff
dablenparty <hdavenport8902@gmail.com>
2021-03-25 14:29:33 -0400

    Added handling for one argument

--- Name From Game CS/Program.cs
+++ Name From Game CS/Program.cs
@@ -1,14 +1,42 @@
 ﻿using System;
+using System.IO;
 using WatcherLibrary;
 using WindowsTools;
 
-namespace Name_From_Game_CS
+namespace NameFromGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var watcher = new Watcher(@"R:\");
+            string savePath = string.Empty,
+                movePath = string.Empty;
+            if (args.Length == 0)
+            {
+                savePath = @".\";
+                movePath = @".\~Clips";
+                // use current directory
+            } else
+            {
+                switch (args.Length)
+                {
+                    case 1:
+                        if (Directory.Exists(args[0]))
+                        {
+                            savePath = args[0];
+                            movePath = Path.Combine(savePath, "~Clips");
+                        }
+                        break;
+
+                    case 2:
+                    // both directories given
+
+                    default:
+                        // display usage
+                        break;
+                }
+            }
+            var watcher = new Watcher(savePath);
             watcher.EnableRaisingEvents = true; // Starts the watcher
             string name;
             while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
@@ -16,7 +44,7 @@ namespace Name_From_Game_CS
                 name = User32Dll.GetActiveWindowTitle(80);
                 if (name != null)
                 {
-                    Console.Write($"\r{name.PadRight(80)}");
+                    //Console.Write($"\r{name.PadRight(80)}");
                 }
             }
             watcher.EnableRaisingEvents = false; // Stops the watcher
