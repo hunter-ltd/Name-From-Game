cdf3e396a18a89b433e7402107cbce37bde8365e
dablenparty <hdavenport8902@gmail.com>
2021-04-12 13:14:42 -0400

    Added write entry methods

--- WatcherLib/RenameTable.cs
+++ WatcherLib/RenameTable.cs
@@ -6,27 +6,59 @@ namespace WatcherLib
 {
     public class RenameTable : Dictionary<string, string>
     {
+        private string _tablePath;
+
+        public string TablePath
+        {
+            get => _tablePath;
+        }
+
         /// <summary>
         /// Creates a rename table from a given file
         /// </summary>
         /// <param name="tablePath">File path to the rename data</param>
         public RenameTable(string tablePath)
         {
-            if (!File.Exists(tablePath))
+            this._tablePath = tablePath;
+            if (!File.Exists(TablePath))
             {
-                File.CreateText(tablePath).Close();
+                File.CreateText(TablePath).Close();
             }
 
-            string[] lines = File.ReadAllLines(tablePath);
+            string[] lines = File.ReadAllLines(TablePath);
             for (int i = 0; i < lines.Length; i+=2)
             {
                 this.Add(lines[i], lines[i + 1]);
             }
         }
 
-        public void WriteNewName(string name)
+        /// <summary>
+        /// Synchronously writes a single new entry into the rename table file
+        /// </summary>
+        /// <param name="name">Entry name parameter</param>
+        public void WriteNewEntry(string name)
         {
-            // TODO write the name then 2 newlines (e.g. $"{name}\n\n") to have the name then a blank line beneath
+            StreamWriter writer = new StreamWriter(TablePath, append: true);
+            foreach (string line in new string[] {name, "", ""})
+            {
+                writer.WriteLine(line);
+            }
+            writer.Close();
+        }
+
+        /// <summary>
+        /// Asynchronously writes a single new entry into the rename table file
+        /// </summary>
+        /// <param name="name">Entry name parameter</param>
+        /// <returns></returns>
+        public async void WriteNewEntryAsync(string name)
+        {
+            StreamWriter writer = new StreamWriter(TablePath, append: true);
+            foreach (string line in new string[] {name, "", ""})
+            {
+                await writer.WriteLineAsync(line);
+            }
+            writer.Close();
         }
     }
 }
