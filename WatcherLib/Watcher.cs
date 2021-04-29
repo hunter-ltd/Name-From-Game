479931d23cb1f174d7bd67a9e9c4914a1103fdaa
dablenparty <hdavenport8902@gmail.com>
2021-04-29 17:20:43 -0400

    Used Tasks to thread the file moving

--- WatcherLib/Watcher.cs
+++ WatcherLib/Watcher.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 using WindowsTools;
 
 namespace WatcherLib
@@ -18,6 +21,9 @@ namespace WatcherLib
         public string MovePath { get; set; }
         
         public RenameTable RenameTable { get; }
+        public Task MoveTask { get; }
+
+        private readonly LinkedList<FileInfo> _fileInfos = new();
 
         /// <summary>
         /// Instantiates a <see cref="Watcher"/> with a given directory, watching for changes in all files.
@@ -36,15 +42,15 @@ namespace WatcherLib
         public Watcher(string watchDirectory, string moveToDirectory, string filter) : base(watchDirectory, filter)
         {
             MovePath = moveToDirectory;
-            Logger _ = new Logger(watchDirectory);
+            Logger _ = new(watchDirectory);
 
             NotifyFilter = NotifyFilters.Attributes
-                                 | NotifyFilters.CreationTime
-                                 | NotifyFilters.DirectoryName
-                                 | NotifyFilters.FileName
-                                 | NotifyFilters.LastAccess
-                                 | NotifyFilters.LastWrite
-                                 | NotifyFilters.Size;
+                           | NotifyFilters.CreationTime
+                           | NotifyFilters.DirectoryName
+                           | NotifyFilters.FileName
+                           | NotifyFilters.LastAccess
+                           | NotifyFilters.LastWrite
+                           | NotifyFilters.Size;
 
             Changed += OnChanged;
             Error += OnError;
@@ -52,6 +58,12 @@ namespace WatcherLib
             RenameTable = new RenameTable(System.IO.Path.Combine(
                 System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), 
                 "names.rmap"));
+            
+            MoveTask = Task.Run(() =>
+            {
+                MoveFromQueue();
+                Thread.Sleep(2000); // every 2 seconds, try to clear out the list
+            });
         }
 
         /// <summary>
@@ -64,7 +76,7 @@ namespace WatcherLib
             try
             {
                 // Tries to open a read-only stream on the file.
-                using FileStream stream = file.OpenRead();
+                using var stream = file.OpenRead();
                 stream.Close();
             } catch (IOException)
             {
@@ -81,7 +93,16 @@ namespace WatcherLib
         /// <returns>Boolean representing if the file is a video or not</returns>
         public static bool IsFileVideo(FileInfo file)
         {
-            string[] videoExtensions = { ".mp4", ".flv", ".mov", ".mkv", ".ts", ".m3u8", ".avi" }; // common video extensions
+            var videoExtensions = new[]
+            {
+                ".mp4",
+                ".flv",
+                ".mov",
+                ".mkv",
+                ".ts",
+                ".m3u8",
+                ".avi"
+            }; // common video extensions
             return videoExtensions.Contains(file.Extension);
         }
 
@@ -91,44 +112,46 @@ namespace WatcherLib
             {
                 return;
             }
-            var file = new FileInfo(e.FullPath);
+            _fileInfos.AddLast(new FileInfo(e.FullPath));
+        }
 
-            while (IsFileVideo(file))
+        private void MoveFromQueue()
+        {
+            if (_fileInfos.Count == 0) return; // Do nothing if the LinkedList is empty
+            foreach (var file in _fileInfos.Where(IsFileAccessible))
             {
-                if (IsFileAccessible(file))
+                Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}] File being moved...");
+                var windowTitle = Regex.Replace(User32Dll.GetActiveWindowTitle(80),
+                    $@"[{new string(System.IO.Path.GetInvalidFileNameChars()) + new string(System.IO.Path.GetInvalidPathChars())}]",
+                    "");
+                Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}]\tWindow title: {windowTitle}");
+
+                windowTitle = RenameTable.TryAdd(windowTitle, windowTitle) ? windowTitle : RenameTable[windowTitle];
+
+                var uniqueMovePath = System.IO.Path.Combine(MovePath, windowTitle);
+                Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}]\tOld path: {file.FullName}");
+
+                Directory.CreateDirectory(uniqueMovePath);
+                uniqueMovePath = System.IO.Path.Combine(uniqueMovePath, windowTitle + " " + file.Name);
+                Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}]\tNew path: {uniqueMovePath}");
+                var moveSuccessMessage = string.Empty;
+                try
                 {
-                    Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}] File being moved...");
-                    string windowTitle = Regex.Replace(User32Dll.GetActiveWindowTitle(80), 
-                        $@"[{new string(System.IO.Path.GetInvalidFileNameChars()) + new string(System.IO.Path.GetInvalidPathChars())}]", "");
-                    Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}]\tWindow title: {windowTitle}");
-
-                    windowTitle = RenameTable.TryAdd(windowTitle, windowTitle) ? windowTitle : RenameTable[windowTitle];
-                    
-                    string uniqueMovePath = System.IO.Path.Combine(MovePath, windowTitle);
-                    Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}]\tOld path: {file.FullName}");
-
-                    Directory.CreateDirectory(uniqueMovePath);
-                    uniqueMovePath = System.IO.Path.Combine(uniqueMovePath, windowTitle + " " + file.Name);
-                    Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}]\tNew path: {uniqueMovePath}");
-                    string moveSuccess = string.Empty;
-                    try 
-                    {
-                        File.Move(file.FullName, uniqueMovePath);
-                        moveSuccess = "File moved successfully!";
-                    } 
-                    catch (IOException ex) 
-                    {
-                        moveSuccess = "File move failed.";
-                        Console.WriteLine(ex.Message);
-                        Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}]\tERROR: {ex.Message}");
-                    } 
-                    finally
-                    {
-                        Console.WriteLine($"[{DateTime.Now.ToLongTimeString()}] {moveSuccess}");
-                        Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}] {moveSuccess}");
-                        Trace.WriteLine("");
-                    }
-                    break;
+                    File.Move(file.FullName, uniqueMovePath);
+                    _fileInfos.Remove(file);
+                    moveSuccessMessage = $"{file.Name} moved successfully!";
+                }
+                catch (IOException ex)
+                {
+                    moveSuccessMessage = $"{file.Name} move failed.";
+                    Console.WriteLine(ex.Message);
+                    Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}]\tERROR: {ex.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine($"[{DateTime.Now.ToLongTimeString()}] {moveSuccessMessage}");
+                    Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}] {moveSuccessMessage}");
+                    Trace.WriteLine("");
                 }
             }
         }
@@ -138,15 +161,13 @@ namespace WatcherLib
 
         private static void PrintException(Exception? ex)
         {
-            if (ex != null)
-            {
-                Console.WriteLine("A major error occurred with the file system watcher. Check the log in the watch directory for more details");
-                Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}] Message: {ex.Message}");
-                Trace.WriteLine("Stacktrace:");
-                Trace.WriteLine(ex.StackTrace);
-                Trace.WriteLine("");
-                PrintException(ex.InnerException);
-            }
+            if (ex == null) return;
+            Console.WriteLine("A major error occurred with the file system watcher. Check the log in the watch directory for more details");
+            Trace.WriteLine($"[{DateTime.Now.ToLongTimeString()}] Message: {ex.Message}");
+            Trace.WriteLine("Stacktrace:");
+            Trace.WriteLine(ex.StackTrace);
+            Trace.WriteLine("");
+            PrintException(ex.InnerException);
         }
     }
 }
